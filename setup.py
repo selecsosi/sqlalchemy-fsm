cace041a9788c314dfbe5bfa343ecacf4289ac65
Ilja Orlovs <ilja@rsrchx.com>
2018-01-04 17:32:55 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Sqlalchemy FSM',
+      version='1.0',
+      description='Finite state machine field for sqlalchemy',
+      author='Peter',
+      url='https://github.com/dagoof/sqlalchemy-fsm',
+      py_modules=['sqlalchemy_fsm'],
+     )
