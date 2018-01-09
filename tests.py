5a3ac30263563795e9868c3f4155468a9f871277
Ilja Orlovs <ilja@rsrchx.com>
2018-01-09 15:32:01 +0000

    class field expression test

--- tests.py
+++ tests.py
@@ -383,6 +383,43 @@ class AltSyntaxBlogPostTest(unittest.TestCase):
         self.assertEqual(self.model.state, 'post_decorated_publish')
         self.assertEqual(self.model.side_effect, 'SeparatePublishHandler::did_two')
 
+    def test_class_query(self):
+        records = [AltSyntaxBlogPost() for idx in range(10)]
+        hidden_records = records[:2]
+        pre_decorated_published = records[3:5]
+        post_decorated_published = records[-2:]
+
+        [el.hide() for el in hidden_records]
+        [el.pre_decorated_publish() for el in pre_decorated_published]
+        [el.post_decorated_publish() for el in post_decorated_published]
+
+        s = session()
+        s.add_all(records)
+        s.commit()
+
+        all_ids = [el.id for el in records]
+        for (handler, expected_group) in [
+            ('hide', hidden_records),
+            ('pre_decorated_publish', pre_decorated_published),
+            ('post_decorated_publish', post_decorated_published),
+        ]:
+            expected_ids = set(el.id for el in expected_group)
+            attr = getattr(AltSyntaxBlogPost, handler)
+            matching = s.query(AltSyntaxBlogPost).filter(
+                attr(),
+                AltSyntaxBlogPost.id.in_(all_ids),
+            ).all()
+            assert len(matching) == len(expected_group)
+            assert set(el.id for el in matching) == expected_ids
+
+            not_matching = s.query(AltSyntaxBlogPost).filter(
+                attr() == False,
+                AltSyntaxBlogPost.id.in_(all_ids),
+            ).all()
+            assert len(not_matching) == (len(records) - len(expected_group))
+            assert expected_ids.intersection(el.id for el in not_matching) == set(), \
+                expected_ids.intersection(el.id for el in not_matching)
+
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
     unittest.main()
