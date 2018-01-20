67aaf68b6f8b5ef35dd5401ffdbc9d9a3e8142de
Ilja Orlovs <ilja@rsrchx.com>
2018-01-20 09:50:51 +0000

    Pypi mk2

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@
 
 from setuptools import setup
 
-setup(name='Sqlalchemy FSM',
-      version='1.0',
-      description='Finite state machine field for sqlalchemy',
-      author='Peter',
-      url='https://github.com/dagoof/sqlalchemy-fsm',
-      py_modules=['sqlalchemy_fsm'],
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest']
-     )
+setup(
+    name='sqlalchemy_fsm',
+    packages=['sqlalchemy_fsm'],
+    py_modules=['sqlalchemy_fsm'],
+    description='Finite state machine field for sqlalchemy',
+    author='Peter & Ilja',
+    author_email='ilja@wise.fish',
+    version='0.0.1',
+    url='https://github.com/dagoof/sqlalchemy-fsm',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
+)
