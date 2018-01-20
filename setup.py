9d947978304a8fc4169f64a377bdf220a280c52a
Ilja Orlovs <ilja@rsrchx.com>
2018-01-20 10:15:34 +0000

    Including requirements txt

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def get_production_requirements():
     """Returns list of production requirements from ./requirements/production.txt"""
-    with open(os.path.join(THIS_DIR, "requirements", "production.txt"), "r") as fobj:
+    with open(os.path.join(THIS_DIR, 'requirements', 'production.txt'), 'r') as fobj:
         return [
             line.strip()
             for line in fobj.readlines()
@@ -21,8 +21,14 @@ setup(
     description='Finite state machine field for sqlalchemy',
     author='Peter & Ilja',
     author_email='ilja@wise.fish',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/dagoof/sqlalchemy-fsm',
+    package_data={
+        'sqlalchemy_fsm': [
+            os.path.join(os.curdir, 'requirements', 'production.txt')
+        ]
+    },
+    include_package_data=True,
     install_requires=get_production_requirements(),
     setup_requires=['pytest-runner'],
     tests_require=['pytest']
