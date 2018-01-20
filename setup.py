68bdbf2b93b5f44eb9c582dfd4ac823caaac7344
Ilja Orlovs <ilja@rsrchx.com>
2018-01-20 10:04:46 +0000

    Adding production deps

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
+import os
 
 from setuptools import setup
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def get_production_requirements():
+    """Returns list of production requirements from ./requirements/production.txt"""
+    with open(os.path.join(THIS_DIR, "requirements", "production.txt"), "r") as fobj:
+        return [
+            line.strip()
+            for line in fobj.readlines()
+            if line.strip()
+        ]
 
 setup(
     name='sqlalchemy_fsm',
@@ -9,8 +21,9 @@ setup(
     description='Finite state machine field for sqlalchemy',
     author='Peter & Ilja',
     author_email='ilja@wise.fish',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/dagoof/sqlalchemy-fsm',
+    install_requires=get_production_requirements(),
     setup_requires=['pytest-runner'],
     tests_require=['pytest']
 )
