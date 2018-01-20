f0d4179a04d24076697ce1798620e213573b476e
Ilja Orlovs <ilja@rsrchx.com>
2018-01-20 10:34:21 +0000

    Ok, I give up - explicit list it is.

--- setup.py
+++ setup.py
@@ -3,16 +3,6 @@ import os
 
 from setuptools import setup
 
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-
-def get_production_requirements():
-    """Returns list of production requirements from ./requirements/production.txt"""
-    with open(os.path.join(THIS_DIR, 'requirements', 'production.txt'), 'r') as fobj:
-        return [
-            line.strip()
-            for line in fobj.readlines()
-            if line.strip()
-        ]
 
 setup(
     name='sqlalchemy_fsm',
@@ -23,13 +13,7 @@ setup(
     author_email='ilja@wise.fish',
     version='0.0.3',
     url='https://github.com/dagoof/sqlalchemy-fsm',
-    package_data={
-        'sqlalchemy_fsm': [
-            os.path.join(os.curdir, 'requirements', 'production.txt')
-        ]
-    },
-    include_package_data=True,
-    install_requires=get_production_requirements(),
+    install_requires=['SQLAlchemy>=1.0.0'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest']
 )
