bd087b1dc57da0a3e41571b25f1b45333b2895b2
Ilja Orlovs <ilja@rsrchx.com>
2018-01-20 12:04:45 +0000

    New release on 2018-01-20 12:04:45.597956: 0.0.8

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='Finite state machine field for sqlalchemy',
     author='Peter & Ilja',
     author_email='ilja@wise.fish',
-    version='0.0.7',
+    version='0.0.8',
     url='https://github.com/dagoof/sqlalchemy-fsm',
     install_requires=['SQLAlchemy>=1.0.0'],
     setup_requires=['pytest-runner'],
