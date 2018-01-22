675ae0a6b82b08b964ad5713a6f786e834cb6da2
Ilja Orlovs <ilja@rsrchx.com>
2018-01-22 09:26:13 +0000

    New release on 2018-01-22 09:26:13.845245: 1.1.7

--- setup.py
+++ setup.py
@@ -38,10 +38,10 @@ setup(
         'Topic :: Database',
     ],
     keywords='sqlalchemy finite state machine fsm',
-    version='1.1.6',
+    version='1.1.7',
     url='https://github.com/VRGhost/sqlalchemy-fsm',
     install_requires=[
-        'SQLAlchemy>=1.1.6',
+        'SQLAlchemy>=1.1.5',
         'six>=1.0.0',
     ],
     setup_requires=['pytest-runner'],
